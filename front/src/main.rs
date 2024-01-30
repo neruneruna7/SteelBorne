952cf10df21925d8278b42599b72204d5f7272d4
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2024-01-30 19:42:12 +0900

    add dioxus

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,8 +1,12 @@
 #![allow(non_snake_case)]
+mod components;
+
+use components::{Footer, Header};
 // import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
 use dioxus::prelude::*;
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::default().module_prefix("front"));
     // launch the web app
     dioxus_web::launch(App);
 }
@@ -10,9 +14,13 @@ fn main() {
 // create a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
     cx.render(rsx! {
-        div {
-            "Hello, world!"
+        main {
+            class: "relative z-0 bg-blue-100 w-screen h-auto min-h-screen flex flex-col justify-start items-stretch",
+            Header {}
+            section {
+                class: "md:container md:mx-auto md:py-8 flex-1",
+            }
+            Footer {}
         }
     })
 }
-
