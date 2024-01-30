7936b88e36fd45aa0aa9d156cc082224fdf8b4a5
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2024-01-30 18:22:39 +0900

    template

---
+++ README.md
@@ -0,0 +1,5 @@
+# Steel Borne
+
+# memo
+shuttleが0.37だとどうにもうごかない（asyncまわりの問題？）
+現状0.36に落として使う
---
+++ api/shuttle/src/main.rs
@@ -0,0 +1,31 @@
+use actix_web::{get, web::ServiceConfig, HttpResponse, Responder, Result};
+use shuttle_actix_web::ShuttleActixWeb;
+
+
+use askama::Template;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    content: String,
+}
+
+#[get("/")]
+async fn index() -> Result<impl Responder> {
+    let index = IndexTemplate {
+        content: "Hello, world!".to_string(),
+    };
+    let renderd = index.render().unwrap();
+
+    Ok(HttpResponse::Ok().body(renderd))
+}
+
+#[shuttle_runtime::main]
+async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let config = move |cfg: &mut ServiceConfig| {
+        cfg.service(index);
+    };
+
+    Ok(config.into())
+}
