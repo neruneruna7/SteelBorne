a0c114f382cec30e6690692879675d898d93f1b9
neruneruna7 <neruneruna7@gmail.com>
2024-03-27 18:32:40 +0900

    update: title component

--- front/src/components/title.rs
+++ front/src/components/title.rs
@@ -3,13 +3,12 @@ use dioxus::prelude::*;
 pub fn Title(cx: Scope) -> Element {
     // でかでかとしたSteel Borne
     cx.render(rsx!(
-        header {
-            class: "text-slate-100 text-8xl bg-zinc-600",
-            div { class: "container mx-auto flex flex-wrap p-0 flex-col md:flex-row justify-between items-center",
+            div { class: "bg-zinc-600 w-full py-24 flex flex-wrap flex-col md:flex-row justify-center items-center",
                 h1 {
+                    class: "text-slate-100 text-8xl text-center",
                     "Steel Borne"
                 }
             }
-        }
+        
     ))
 }
--- front/src/main.rs
+++ front/src/main.rs
@@ -26,7 +26,6 @@ fn App(cx: Scope) -> Element {
             h1 { "High-Five counter: {count}" }
             button { onclick: move |_| count += 1, "Up high!" }
             button { onclick: move |_| count -= 1, "Down low!" }
-            Footer {}
         }
     })
 }
