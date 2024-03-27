70de78a6796e2dd2c7fa36f30c661a184ecca975
neruneruna7 <neruneruna7@gmail.com>
2024-03-27 20:57:41 +0900

    update: selector component ux

--- front/src/components/selector.rs
+++ front/src/components/selector.rs
@@ -13,13 +13,14 @@ pub fn Selector(cx: Scope<SelectorProps>) -> Element {
     // let navigate_to = &;
     cx.render(rsx!(
             div { class: "bg-inherit  w-full my-4 px-2 flex flex-wrap flex-col md:flex-row justify-center items-center",
+            Link {
+                to: match cx.props.navigate_to {
+                    Route::Home => Route::Home {},
+                    Route::Profile => Route::Profile {},
+                },
                 div {
                     class: "w-48 p-1 hover:bg-gradient-to-r from-neutral-950 from-0% via-neutral-700 via-50% to-neutral-950",
-                    Link {
-                        to: match cx.props.navigate_to {
-                            Route::Home => Route::Home {},
-                            Route::Profile => Route::Profile {},
-                        },
+                    
                         h1 {
                             class: "text-slate-100 text-2xl text-center",
                             "{text}"
--- front/src/pages/home.rs
+++ front/src/pages/home.rs
@@ -4,8 +4,6 @@ use crate::components::{Counter, Selector, Title};
 use crate::Route;
 
 pub fn Home(cx: Scope) -> Element {
-    let mut count = use_state(cx, || 0);
-
     cx.render(rsx! {
         div {
             class: "relative z-0 bg-neutral-950 w-screen h-auto min-h-screen flex flex-col justify-start items-stretch",
@@ -16,9 +14,6 @@ pub fn Home(cx: Scope) -> Element {
             section {
                 class: "md:container md:mx-auto md:py-8 flex-1",
             }
-            h1 { "High-Five counter: {count}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
         }
     })
 }
