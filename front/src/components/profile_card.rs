bd14c4e9fc3a4c013999f065ed8c02cf04bfa113
neruneruna7 <neruneruna7@gmail.com>
2024-03-27 23:43:23 +0900

    add: profile card

--- front/src/components/profile_card.rs
+++ front/src/components/profile_card.rs
@@ -1,6 +1,4 @@
-use crate::Route;
 use dioxus::prelude::*;
-use dioxus_router::components::Link;
 
 #[derive(PartialEq, Props, Clone)]
 pub struct ProfileCardProps {
@@ -11,7 +9,12 @@ pub struct ProfileCardProps {
 pub fn ProfileCard(cx: Scope<ProfileCardProps>) -> Element {
     cx.render(rsx!(
         div {
-            class: "border-2 border-gray-600",
+            class: "border-2 border-gray-600, w-full p-10",
+            div {
+                class: "border-2 w-full h-full p-7 bg-slate-900",
+                Name {name: String::from("621")}
+                ProfileImage {icon_image_path: cx.props.icon_image_path.clone()}
+            }
         }
     ))
 }
@@ -23,11 +26,39 @@ pub struct NameProps {
 }
 
 #[component]
-
-fn Name(cx: Scope<ProfileCardProps>) -> Element {
+fn Name(cx: Scope<NameProps>) -> Element {
     cx.render(rsx!(
         div {
-            class: "text-slate-100 ",
+            class: "w-full p-1",
+            p {
+                class: "text-slate-100 text-lg text-start",
+                "Name"
+            }
+            h1 {
+                class: "text-slate-100 text-2xl text-start",
+                cx.props.name.clone()
+            }
+        }
+    ))
+}
+
+#[derive(PartialEq, Props, Clone)]
+pub struct ProfileImageProps {
+    icon_image_path: String,
+}
+
+#[component]
+pub fn ProfileImage(cx: Scope<ProfileImageProps>) -> Element {
+    let image_path =  cx.props.icon_image_path.as_str();
+    cx.render(rsx!(
+        div {
+            class: "w-1/2 h-auto m-4 border-black border-4  overflow-hidden",
+            img {
+                class: "w-auto h-auto",
+                alt: "profile image",
+                src: image_path,
+                "loading": "lazy"
+            }
         }
     ))
 }
--- front/src/pages/profile.rs
+++ front/src/pages/profile.rs
@@ -9,13 +9,7 @@ pub fn Profile(cx: Scope) -> Element {
         main {
             class: "relative z-0 bg-neutral-950 w-screen h-auto min-h-screen flex flex-row justify-start items-stretch",
             NavigationBar { now_route: Route::Profile {} }
-            ProfileCard{icon_image_path: String::from("")}
-
-            // Selector { text:"LANGUAGE".to_string() }
-            Counter {}
-            section {
-                class: "md:container md:mx-auto md:py-8 flex-1",
-            }
+            ProfileCard{icon_image_path: String::from("user_icon.jpg")}
         }
 
     })
