87ee821d973058e083cfa3f7c8a32f703f13a5a1
neruneruna7 <neruneruna7@gmail.com>
2024-04-15 19:49:56 +0900

    fmt

--- front/src/components.rs
+++ front/src/components.rs
@@ -4,14 +4,13 @@ mod header;
 mod navigation_bar;
 mod profile_card;
 mod selector;
-mod title;
 mod sns_link_card;
+mod title;
 
 pub use counter::Counter;
-
 
 pub use navigation_bar::NavigationBar;
 pub use profile_card::ProfileCard;
 pub use selector::Selector;
-pub use title::Title;
 pub use sns_link_card::SnsLinkCard;
+pub use title::Title;
--- front/src/main.rs
+++ front/src/main.rs
@@ -2,7 +2,6 @@
 mod components;
 mod pages;
 
-
 // import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
 use dioxus::prelude::*;
 use dioxus_router::prelude::*;
